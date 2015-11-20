4e5605f8225eb135ede1b9ca9a63c7e7c18bdab1
Steven Byle <steven.byle@gmail.com>
2015-11-20 11:28:50 -0500

    Updated support libraries. Converted hardcoded text to string resources.

--- MaterialThemesMadeEasy/app/src/main/java/com/stevenbyle/android/materialthemes/controller/home/material/MaterialThemeInXmlFragment.java
+++ MaterialThemesMadeEasy/app/src/main/java/com/stevenbyle/android/materialthemes/controller/home/material/MaterialThemeInXmlFragment.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.internal.view.ContextThemeWrapper;
+import android.support.v7.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
