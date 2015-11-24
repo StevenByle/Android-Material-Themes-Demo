a5448b01cb2c7c333287fe24feeea020c5ffe24e
Steven Byle <steven.byle@gmail.com>
2015-11-24 15:27:22 -0500

    Added more theme sets from utils. Made selectable areas wrap content.

--- MaterialThemesMadeEasy/app/src/main/java/com/stevenbyle/android/materialthemes/controller/theme/MaterialThemeUtils.java
+++ MaterialThemesMadeEasy/app/src/main/java/com/stevenbyle/android/materialthemes/controller/theme/MaterialThemeUtils.java
@@ -12,9 +12,9 @@ public class MaterialThemeUtils {
     // App themes
     public static final MaterialTheme THEME_ORANGE =
             new MaterialTheme(R.string.material_theme_orange, R.style.AppTheme_Orange);
-    public static final MaterialTheme THEME_BLUE =
-            new MaterialTheme(R.string.material_theme_green, R.style.AppTheme_Green);
     public static final MaterialTheme THEME_GREEN =
+            new MaterialTheme(R.string.material_theme_green, R.style.AppTheme_Green);
+    public static final MaterialTheme THEME_BLUE =
             new MaterialTheme(R.string.material_theme_blue, R.style.AppTheme_Blue);
 
     // Dialog themes
@@ -34,13 +34,37 @@ public class MaterialThemeUtils {
             new MaterialTheme(R.string.material_theme_blue, R.style.AlertDialogTheme_Blue);
 
     private static List<MaterialTheme> sThemeList;
+    private static List<MaterialTheme> sDialogThemeList;
+    private static List<MaterialTheme> sAlertDialogThemeList;
 
     public static List<MaterialTheme> getThemeList() {
         if (sThemeList == null) {
             sThemeList = new ArrayList<MaterialTheme>();
             sThemeList.add(THEME_ORANGE);
-            sThemeList.add(THEME_BLUE);
             sThemeList.add(THEME_GREEN);
+            sThemeList.add(THEME_BLUE);
+        }
+
+        return sThemeList;
+    }
+
+    public static List<MaterialTheme> getDialogThemeList() {
+        if (sDialogThemeList == null) {
+            sDialogThemeList = new ArrayList<MaterialTheme>();
+            sDialogThemeList.add(THEME_DIALOG_ORANGE);
+            sDialogThemeList.add(THEME_DIALOG_GREEN);
+            sDialogThemeList.add(THEME_DIALOG_BLUE);
+        }
+
+        return sThemeList;
+    }
+
+    public static List<MaterialTheme> getAlertDialogThemeList() {
+        if (sAlertDialogThemeList == null) {
+            sAlertDialogThemeList = new ArrayList<MaterialTheme>();
+            sAlertDialogThemeList.add(THEME_ALERT_DIALOG_ORANGE);
+            sAlertDialogThemeList.add(THEME_ALERT_DIALOG_GREEN);
+            sAlertDialogThemeList.add(THEME_ALERT_DIALOG_BLUE);
         }
 
         return sThemeList;
