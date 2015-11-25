9e681285a243461969f4d1fe2ed57d906a4d8080
Steven Byle <steven.byle@gmail.com>
2015-11-25 10:34:28 -0500

    Added more themes.

--- MaterialThemesMadeEasy/app/src/main/java/com/stevenbyle/android/materialthemes/controller/theme/MaterialThemeUtils.java
+++ MaterialThemesMadeEasy/app/src/main/java/com/stevenbyle/android/materialthemes/controller/theme/MaterialThemeUtils.java
@@ -10,28 +10,52 @@ import java.util.List;
  */
 public class MaterialThemeUtils {
     // App themes
+    public static final MaterialTheme THEME_RED =
+            new MaterialTheme(R.string.material_theme_red, R.style.AppTheme_Red);
     public static final MaterialTheme THEME_ORANGE =
             new MaterialTheme(R.string.material_theme_orange, R.style.AppTheme_Orange);
+    public static final MaterialTheme THEME_YELLOW =
+            new MaterialTheme(R.string.material_theme_yellow, R.style.AppTheme_Yellow);
     public static final MaterialTheme THEME_GREEN =
             new MaterialTheme(R.string.material_theme_green, R.style.AppTheme_Green);
+    public static final MaterialTheme THEME_TEAL =
+            new MaterialTheme(R.string.material_theme_teal, R.style.AppTheme_Teal);
     public static final MaterialTheme THEME_BLUE =
             new MaterialTheme(R.string.material_theme_blue, R.style.AppTheme_Blue);
+    public static final MaterialTheme THEME_PURPLE =
+            new MaterialTheme(R.string.material_theme_purple, R.style.AppTheme_Purple);
 
     // Dialog themes
+    public static final MaterialTheme THEME_DIALOG_RED =
+            new MaterialTheme(R.string.material_theme_red, R.style.DialogTheme_Red);
     public static final MaterialTheme THEME_DIALOG_ORANGE =
             new MaterialTheme(R.string.material_theme_orange, R.style.DialogTheme_Orange);
+    public static final MaterialTheme THEME_DIALOG_YELLOW =
+            new MaterialTheme(R.string.material_theme_yellow, R.style.DialogTheme_Yellow);
     public static final MaterialTheme THEME_DIALOG_GREEN =
             new MaterialTheme(R.string.material_theme_green, R.style.DialogTheme_Green);
+    public static final MaterialTheme THEME_DIALOG_TEAL =
+            new MaterialTheme(R.string.material_theme_teal, R.style.DialogTheme_Teal);
     public static final MaterialTheme THEME_DIALOG_BLUE =
             new MaterialTheme(R.string.material_theme_blue, R.style.DialogTheme_Blue);
+    public static final MaterialTheme THEME_DIALOG_PURPLE =
+            new MaterialTheme(R.string.material_theme_purple, R.style.DialogTheme_Purple);
 
     // Alert dialog themes
+    public static final MaterialTheme THEME_ALERT_DIALOG_RED =
+            new MaterialTheme(R.string.material_theme_red, R.style.AlertDialogTheme_Red);
     public static final MaterialTheme THEME_ALERT_DIALOG_ORANGE =
             new MaterialTheme(R.string.material_theme_orange, R.style.AlertDialogTheme_Orange);
+    public static final MaterialTheme THEME_ALERT_DIALOG_YELLOW =
+            new MaterialTheme(R.string.material_theme_yellow, R.style.AlertDialogTheme_Yellow);
     public static final MaterialTheme THEME_ALERT_DIALOG_GREEN =
             new MaterialTheme(R.string.material_theme_green, R.style.AlertDialogTheme_Green);
+    public static final MaterialTheme THEME_ALERT_DIALOG_TEAL =
+            new MaterialTheme(R.string.material_theme_teal, R.style.AlertDialogTheme_Teal);
     public static final MaterialTheme THEME_ALERT_DIALOG_BLUE =
             new MaterialTheme(R.string.material_theme_blue, R.style.AlertDialogTheme_Blue);
+    public static final MaterialTheme THEME_ALERT_DIALOG_PURPLE =
+            new MaterialTheme(R.string.material_theme_purple, R.style.AlertDialogTheme_Purple);
 
     private static List<MaterialTheme> sThemeList;
     private static List<MaterialTheme> sDialogThemeList;
@@ -40,33 +64,42 @@ public class MaterialThemeUtils {
     public static List<MaterialTheme> getThemeList() {
         if (sThemeList == null) {
             sThemeList = new ArrayList<MaterialTheme>();
+            sThemeList.add(THEME_RED);
             sThemeList.add(THEME_ORANGE);
+            sThemeList.add(THEME_YELLOW);
             sThemeList.add(THEME_GREEN);
+            sThemeList.add(THEME_TEAL);
             sThemeList.add(THEME_BLUE);
+            sThemeList.add(THEME_PURPLE);
         }
-
         return sThemeList;
     }
 
     public static List<MaterialTheme> getDialogThemeList() {
         if (sDialogThemeList == null) {
             sDialogThemeList = new ArrayList<MaterialTheme>();
+            sDialogThemeList.add(THEME_DIALOG_RED);
             sDialogThemeList.add(THEME_DIALOG_ORANGE);
+            sDialogThemeList.add(THEME_DIALOG_YELLOW);
             sDialogThemeList.add(THEME_DIALOG_GREEN);
+            sDialogThemeList.add(THEME_DIALOG_TEAL);
             sDialogThemeList.add(THEME_DIALOG_BLUE);
+            sDialogThemeList.add(THEME_DIALOG_PURPLE);
         }
-
-        return sThemeList;
+        return sDialogThemeList;
     }
 
     public static List<MaterialTheme> getAlertDialogThemeList() {
         if (sAlertDialogThemeList == null) {
             sAlertDialogThemeList = new ArrayList<MaterialTheme>();
+            sAlertDialogThemeList.add(THEME_ALERT_DIALOG_RED);
             sAlertDialogThemeList.add(THEME_ALERT_DIALOG_ORANGE);
+            sAlertDialogThemeList.add(THEME_ALERT_DIALOG_YELLOW);
             sAlertDialogThemeList.add(THEME_ALERT_DIALOG_GREEN);
+            sAlertDialogThemeList.add(THEME_ALERT_DIALOG_TEAL);
             sAlertDialogThemeList.add(THEME_ALERT_DIALOG_BLUE);
+            sAlertDialogThemeList.add(THEME_ALERT_DIALOG_PURPLE);
         }
-
-        return sThemeList;
+        return sAlertDialogThemeList;
     }
 }
